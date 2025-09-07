7d2677863015488593ae63939ae7fc5fbea8c92a
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2025-09-07 13:02:57 +0100

    Implement Home Filters

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 
 import ROUTES from "@/constants/routes";
-import { Button } from "@/components/ui/button";
 import LocalSearch from "@/components/search/LocalSearch";
+import HomeFilter from "@/components/filters/HomeFilter";
+import { Button } from "@/components/ui/button";
 
 const questions = [
   {
@@ -54,11 +55,17 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(query?.toLowerCase()),
-  );
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query.toLowerCase());
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
@@ -79,7 +86,7 @@ const Home = async ({ searchParams }: SearchParams) => {
           otherClasses="flex-1"
         />
       </section>
-      {/* HomeFilter */}
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>
