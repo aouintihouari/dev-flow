68f6acb9cb267365fea4bedb09831cdc0b7681c3
Houari Aouinti <169095690+aouintihouari@users.noreply.github.com>
2025-09-10 15:22:04 +0100

    Implement Account by provider

---
+++ src/app/api/accounts/provider/route.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { AccountSchema } from "@/lib/validations";
+import Account from "../../../../../database/account.model";
+import { extractFieldErrors } from "@/helpers/flatten";
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+
+    if (!validatedData.success) {
+      const tree = z.treeifyError(validatedData.error);
+      const fieldErrors = extractFieldErrors(tree);
+      throw new ValidationError(fieldErrors);
+    }
+
+    const account = await Account.findOne({ providerAccountId });
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json(
+      {
+        success: true,
+        data: account,
+      },
+      { status: 200 },
+    );
+  } catch (err) {
+    return handleError(err, "api") as APIErrorResponse;
+  }
+}
